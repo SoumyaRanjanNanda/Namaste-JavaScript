45576b0fa46228cc368fd17ccab24bc7009e9f67
Soumya Ranjan Nanda <222658249+SoumyaRanjanNanda@users.noreply.github.com>
2026-03-19 15:55:33 +0530

    Create 01-DataTypes.js
    data types in js primitive and nonprimitive

---
+++ 03-DataTypes/01-DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as new version
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(3
+          +
+             3) // code readability should be high
+console.log("Soumya")
+
+// data types ( premetive data types)
+let name = "soumya"
+let age = 20
+let isLoggedin = false
+// number => 2 to power 53
+// bigint =>
+// string => " "
+// boolean => true / false
+// null => standalone value
+// undefined => not assingend value // let state;
+// symbol => unique 
+
+//data types (non premitive)
+// object
+console.log(typeof "soumya"); // output => string
+console.log(typeof age); // output => number
+console.log(typeof null); // ouput => object
+console.log(typeof undefined); // output => undefined
+
