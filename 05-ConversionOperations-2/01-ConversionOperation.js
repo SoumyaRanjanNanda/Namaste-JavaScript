0e16702f4328627f6fc5faf6588a9f67bb5159dc
Soumya Ranjan Nanda <222658249+SoumyaRanjanNanda@users.noreply.github.com>
2026-03-25 23:35:22 +0530

    Create 01-ConversionOperation.js
    Conversion And Operation

---
+++ 05-ConversionOperations-2/01-ConversionOperation.js
@@ -0,0 +1,40 @@
+let value = 3;
+let negValue= -value;
+console.log(negValue); // output => -3
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);// output => 8
+console.log(2/2);
+console.log(2%2); //  uses in cryptography
+
+let str1= "hello";
+let str2 = " Soumya";
+let str3= str1 + str2;
+console.log(str3); // output =>  hello Soumya
+console.log("1" + 2); // output => 12
+console.log(1 + "2"); // output => 12
+console.log("1" + 2 + 2); // output => 122 (All depends upoun ECMAScript)
+console.log(1 + 2 + "2"); // output => 32 (All depends upoun ECMAScript)
+// console.log( 3 + 4 * 5 % 3); intend of this use this which you want to do first in bracket console.log( (3 + 4) * 5 % 3);
+
+// tricky conversion
+
+console.log(true); // output => true
+console.log(+true); // output => 1 
+console.log(+ ""); // output => 0
+
+let num1,num2,num3;
+num1=num2=num3=2+1; // not value able code messy code this is not a good way
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // output => 101
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); // output => 101
+// follow MDN
+
+
+
